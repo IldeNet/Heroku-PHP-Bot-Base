3fdba8b566e6cdbfe2dbbeb1cee4f83272bcf2a7
IldeNet <ildenet@hotmail.com>
2021-10-21 22:38:23 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,6 @@ $var = file_get_contents("php://input");
 $var = json_decode($var,true);
 $chat_id = $var['message']['chat']['id'];
 $text = $var['message']['text'];
-$tipochat = $var->message->chat->type;
 $token = "551082423:AAG9pkYJCW-4BctGLst4PtXLk2u9-GK8vPk"; 
 
 function sendMessage($chat_id,$text)
@@ -63,23 +62,4 @@ if (strpos($text,'bot') !== false)
 	$message = "El bot se encuentra actualmente en mantenimiento. ";
 	echo sendMessage($chat_id,$message);
 }
-if($tipochat == 'private')
-	{
-	$curl = curl_init();
-	$accesstoken = "2b2a75dfdefe40afad8c0e31d2db12c2";
-	$headr = array();
-	$headr[] = 'Content-type: application/json';
-	$header[] = 'Authorization:Bearer '.$accesstoken;
-	$query = str_replace(" ","%20", $msg);
-	curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.dialogflow.com/v1/query?v=20170712&query='.$query.'&lang=es&sessionId=dc25e853-d0f5-5121-2957-6cfe3e0badd3&timezone=Europe/Madrid',
-    CURLOPT_HTTPHEADER => $header
-	));
-	$result = json_decode(curl_exec($curl));
-	$message = $result->result->fulfillment->speech;
-	curl_close($curl);
-	echo sendMessage($chat_id,$message);
-	}
-
 ?>
