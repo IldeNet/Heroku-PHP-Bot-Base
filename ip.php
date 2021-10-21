501d6fec63ec5e7e870bc23f6d0bd0387e96bb61
IldeNet <ildenet@hotmail.com>
2021-10-21 22:23:37 +0200

    Add files via upload

---
+++ ip.php
@@ -0,0 +1,7 @@
+
+<?php 
+echo file_get_contents("http://icanhazip.com/"); echo '<br>';
+echo file_get_contents("http://ifconfig.me/ip"); echo '<br>';
+echo file_get_contents("http://myip.dnsomatic.com/"); echo '<br>';
+echo file_get_contents("http://ip.appspot.com");
+ ?>
